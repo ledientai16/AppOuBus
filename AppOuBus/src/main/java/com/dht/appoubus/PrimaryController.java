a1e307e0dd07ca718b5ae4fe7a16d24b2a442eda
ledientai16 <ledientai16@gmail.com>
2020-11-26 20:27:24 +0700

    Chinh sua UI Primary, them primari controller, them service

--- AppOuBus/src/main/java/com/dht/appoubus/PrimaryController.java
+++ AppOuBus/src/main/java/com/dht/appoubus/PrimaryController.java
@@ -1,12 +1,72 @@
 package com.dht.appoubus;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class PrimaryController {
-
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    public AnchorPane rootPane;
+   
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+       
+    }   
+
+    /**
+     *
+     *  Event power off
+     * Exit APPLiCATION when Click power off
+     * 
+     */
+    public void close (MouseEvent event){
+        Platform.exit();
+        System.exit(0);
+    }
+
+    /**
+     *
+     *  event Change UI
+     * change UI from primary UI
+     */
+    
+    @FXML
+    public void loadUI(String UI) throws IOException{
+       
+        AnchorPane pane = FXMLLoader.load(getClass().getResource(UI));
+        rootPane.getChildren().setAll(pane);
+        
+   }
+        
+    /**
+     *
+     * @param event
+     */
+    @FXML
+    public void loadThemChuyenUI(ActionEvent e){
+        
+        try {
+            loadUI("themchuyen.fxml");
+        } catch (IOException ex) {
+            Logger.getLogger(PrimaryController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public void loadsecond(ActionEvent e){
+        
+        try {
+            loadUI("secondary.fxml");
+        } catch (IOException ex) {
+            Logger.getLogger(PrimaryController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
